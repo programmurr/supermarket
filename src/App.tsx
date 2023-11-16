07a903185e636fa0e0f20df087067a0fef2f332f
Conor Murray <64764695+programmurr@users.noreply.github.com>
2023-11-16 20:25:24 +0000

    Implement error handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import "./App.css";
 import { Product } from "./types";
 
 const productEndpoint =
-  " https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json";
+  "https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch(productEndpoint);
-      const data: Product[] = await response.json();
-      setProducts(data);
+      try {
+        const response = await fetch(productEndpoint);
+        const data: Product[] = await response.json();
+        setProducts(data);
+        setErrorMessage("");
+      } catch (error) {
+        setProducts([]);
+        setErrorMessage("Error fetching products");
+      }
     }
     fetchProducts();
   }, []);
@@ -19,6 +26,7 @@ function App() {
   return (
     <div className="App">
       <ul>
+        {errorMessage && <p>{errorMessage}</p>}
         {products.map((product) => (
           <li>
             <p>{product.name}</p>
