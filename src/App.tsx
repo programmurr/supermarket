7fcc2452c7324aafc5f039fbd7fc61e3901be5b6
Conor Murray <64764695+programmurr@users.noreply.github.com>
2023-11-16 19:59:41 +0000

    Fetch products from API endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
+const productEndpoint =
+  " https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json";
+
 function App() {
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch(productEndpoint);
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchProducts();
+  }, []);
+
   return <div className="App"></div>;
 }
 
